0013ae57dbd08d7b29c43342511cd64ae92feea0
Abigail Godoy <A01709167@tec.mx>
2026-04-25 20:55:36 -0600

    Nuevas reglas y títulos

--- kb_v2.py
+++ kb_v2.py
@@ -18,6 +18,12 @@ EXTRA_FACTS = {
     ("Accesible", "SalaReuniones"),
     ("Centrico", "AulaA"),
     ("Centrico", "SalaReuniones"),
+    ("CapacidadMedia", "AulaA"),
+    ("CapacidadMedia", "SalaReuniones"),
+    ("CapacidadBaja", "AulaB"),
+    ("CapacidadBaja", "Biblio1"),
+    ("CapacidadAlta", "SalaReuniones"),
+    ("CapacidadAlta", "AuditorioMini"),
 }
 
 EXTRA_RULES = [
@@ -29,7 +35,7 @@ EXTRA_RULES = [
         antecedents=(("ReunionAccesible", "?g"),),
         consequent=("ReunionEquipo", "?g"),
         description=(
-            "Toda reunión accesible hereda el tipo ReunionEquipo, "
+            "Para toda reunión, si es accesible, entonces es una Reunión en Equipo."
         ),
     ),
 
@@ -40,8 +46,7 @@ EXTRA_RULES = [
         antecedents=(("ReunionAccesible", "?g"),),
         consequent=("NecesitaAccesibilidad", "?g"),
         description=(
-            "Toda reunión accesible requiere que el espacio asignado "
-            "cuente con el predicado Accesible."
+            "Toda reunión accesible requiere que el espacio asignado sea Accesible."
         ),
     ),
 
@@ -71,27 +76,32 @@ EXTRA_RULES = [
         ),
         consequent=("Recomendable", "?s", "?g", "?t"),
         description=(
-            "Un espacio asignable y céntrico es recomendable para solicitudes de tipo Presentacion."
+            "Un espacio asignable y céntrico es recomendable para solicitudes de tipo Presentación."
         ),
     ),
 
-    ##### Jerarquia de capacidad (MI MON) #####
+    ##### Jerarquía de capacidad (MI MON) #####
         # ∀s  CapacidadAlta(s) ⇒ CapacidadMedia(s)
     Rule(
         name="R13_capacidad_alta_implica_media",
         antecedents=(("CapacidadAlta", "?s"),),
         consequent=("CapacidadMedia", "?s"),
-        description=(
-            "Todo espacio de capacidad alta también tiene capacidad media, haciéndolo elegible para solicitudes medianas."
-        ),
+        description="Todo espacio de capacidad alta también tiene capacidad media, haciéndolo elegible para solicitudes medianas.",
     ),
 
+    ### Recomendación por prioridad de presentación grande ###
+    # ∀g (PresentacionGrande(g) ⇒ AltaPrioridad(g))
     Rule(
         name="R14_prioridad_presentacion",
         antecedents=(("PresentacionGrande", "?g"),),
         consequent=("AltaPrioridad", "?g"),
+        description=(
+            "Las presentaciones grandes tienen alta prioridad."
+        ),
     ),
 
+    ### Recomendación por prioridad de presentación de alta prioridad ###
+    # ∀s∀g∀t(Asignable(s,g,t)∧AltaPrioridad(g)⇒AltamenteRecomendable(s,g,t))
     Rule(
         name="R15_preferir_prioridad",
         antecedents=(
@@ -99,8 +109,11 @@ EXTRA_RULES = [
             ("AltaPrioridad", "?g"),
         ),
         consequent=("AltamenteRecomendable", "?s", "?g", "?t"),
+        description=("Si una solicitud tiene alta prioridad, es altamente recomendable."),
     ),
 
+    ### Recomendación por prioridad de director ###
+    # ∀s∀t(Solicita(Director1,t)∧ Libre(s, t)⇒ AsignablePrioritario(s,Director1,t))
     Rule(
         name="R16_prioridad_director",
         antecedents=(
@@ -111,11 +124,77 @@ EXTRA_RULES = [
         description="El usuario Director1 tiene prioridad sobre espacios libres.",
     ),
 
+    ### Recomendación por continuidad ###
+    # ∀g (Requiere2Horas(g) ⇒ NecesitaSlotsConsecutivos(g))
     Rule(
         name="R17_requiere_continuidad",
         antecedents=(("Requiere2Horas", "?g"),),
         consequent=("NecesitaSlotsConsecutivos", "?g"),
+        description=("Para toda solicitud que requiere 2 horas, necesita slots consecutivos."),
     ),
+
+    # Abigail
+
+    ### Las reuniones individuales son de capacidad baja ###
+    # ∀g  EstudioIndividual(g) ⇒ CapacidadBaja(g)
+    Rule(
+    name="R18_capacidad_alta_implica_media",
+    antecedents=(("EstudioIndividual", "?g"),),
+    consequent=("CapacidadBaja", "?g"),
+    description=(
+        "Todo estudio individual es de capacidad baja."
+    ),
+    ),
+
+    ### Capacidad Media implica capacidad Baja ###
+    # ∀s  CapacidadMedia(s) ⇒ CapacidadBaja(s)
+    Rule(
+        name="R19_capacidad_media_implica_baja",
+        antecedents=(("CapacidadMedia", "?s"),),
+        consequent=("CapacidadBaja", "?s"),
+        description=(
+            "Todo espacio con capacidad media también cumple con capacidad baja."
+        ),
+    ),
+
+    ### Reunion en equipo requiere capacidad Media ###
+    # ∀g  ReunionEquipo(g) ⇒ RequiereCapacidadMedia(g)
+    Rule(
+        name="R20_reunion_equipo_requiere_media",
+        antecedents=(("ReunionEquipo", "?g"),),
+        consequent=("RequiereCapacidadMedia", "?g"),
+        description=(
+            "Toda reunión en equipo requiere al menos un espacio con capacidad media."
+        ),
+    ),
+
+    ### Toda presentación es de capacidad Media ###
+    # ∀g  Presentacion(g) ⇒ CapacidadMedia(g)
+    Rule(
+        name="R21_presentacion_capacidad_media",
+        antecedents=(("Presentacion", "?g"),),
+        consequent=("CapacidadMedia", "?g"),
+        description=(
+            "Toda presentación es de capacidad media."
+        ),
+    ),
+
+    ### Si una solicitud requiere silencio, el espacio debe ser silenciosa ###
+    # ∀s∀t(Solicita(g,t)∧RequiereSilencio(g)⇒Silenciosa(s))
+    Rule(
+        name="R22_asignable_si_requiere_silencio",
+        antecedents=(
+            ("Libre", "?s", "?t"),
+            ("Solicita", "?g", "?t"),
+            ("RequiereSilencio", "?g"),
+            ("Silenciosa", "?s"),
+        ),
+        consequent=("Asignable", "?s", "?g", "?t"),
+        description=(
+            "Si una solicitud requiere silencio, solo espacios silenciosos pueden ser asignables."
+        ),
+    ),
+
 ]
 
 def build_kb() -> dict:
